5c4bc278aa08d6c9b18e2edd786546a87145c6fa
Chavane Minto <chevy@MacBook-Pro-15.dhcp.thefacebook.com>
2017-06-22 11:43:03 -0700

    Customized Cells. Added movie title and overview.

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    //Global variable used to hold dictionary of moives
+    
+    var movies: [[String: Any]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
 
+        //Create Network Request: url, request, session & task
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=fe2f217e9c2c68b9ea9de1fe42905fb0")
         
         let request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -28,17 +37,35 @@ class NowPlayingViewController: UIViewController {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 //grab a dictionary of objects nested inside of the original dictionary. This is a dictionary of "results"
                 let movies = dataDictionary["results"] as! [[String: Any]]
-                //use a for loop to go thruogh the array of objects
-                for movie in movies {
-                    //store the "title" key-value pairs in the array of objects
-                    let title = movie["title"] as! String
-                    print(title)
+                self.movies = movies
+                self.tableView.reloadData()
                 }
-                
-            }
         }
         task.resume()
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //create cell that calls to your custom cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        //grab movie info at index of the array for cell
+        let movie = movies[indexPath.row]
+        //for each movie, the cell will grab 3 things:
+        let title = movie["title"] as! String // grabs the movie title from dictionary
+        let overview = movie["overview"] as! String // grabs the movie overview from dictionary
+        
+        //pass info from dictionaries to the components of custom cell
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
