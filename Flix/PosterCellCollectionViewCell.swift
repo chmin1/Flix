465a1ff623c170e167273c7718ee52fe3ea41c94
Chavane Minto <chevy@fb.com>
2017-06-23 16:31:39 -0700

    Added collection view to new tab

---
+++ Flix/PosterCellCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCellCollectionViewCell.swift
+//  Flix
+//
+//  Created by Chavane Minto on 6/23/17.
+//  Copyright © 2017 Chavane Minto. All rights reserved.
+//
+
+import UIKit
+
+class PosterCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
---
+++ Flix/SuperheroViewController.swift
@@ -0,0 +1,83 @@
+//
+//  SuperheroViewController.swift
+//  Flix
+//
+//  Created by Chavane Minto on 6/23/17.
+//  Copyright © 2017 Chavane Minto. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroViewController: UIViewController, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies: [[String: Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.dataSource = self
+        fetchMovies()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCellCollectionViewCell
+        let movie = movies[indexPath.item]
+        if let posterPathString = movie["poster_path"] as? String {
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            let posterURL = URL(string: baseURLString + posterPathString)!
+            cell.posterImageView.af_setImage(withURL: posterURL)
+        }
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func fetchMovies() {
+        
+        //Create Network Request: url, request, session & task
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=fe2f217e9c2c68b9ea9de1fe42905fb0")
+        
+        let request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                //grab a dictionary of JSON objects. in this case, movie objects.
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                //grab a dictionary of objects nested inside of the original dictionary. This is a dictionary of "results"
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.collectionView.reloadData()
+//                self.refreshControl.endRefreshing()
+//                self.activityIndicator.stopAnimating()
+            }
+        }
+        task.resume()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
